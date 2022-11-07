bb61560f613fe274e44e2fa24376b6dcf93049ce
Ignacio Rodríguez <ignacio.rodriguez024@gmail.com>
2022-11-07 20:02:38 -0300

    Agregado objeto, html, css

--- index.js
+++ index.js
@@ -16,59 +16,80 @@ let netoCompras = 0;
 let netoVentas = 0;
 
 
+
 const categorias = ['categoria A', 'categoria B', 'categoria C', 'categoria D'];
 
 const limites = [748380.07, 1112459.83, 1557443.75, 1934273.04, 2277684.56];
 
+const nombre = prompt("Ingrese su nombre");
+
+const condicionFiscal = prompt("Ingrese su condición fiscal (monotributista o responsable inscripto)");
+
 const facturacion = parseFloat(prompt("Ingrese el valor de lo facturado en el año hasta la fecha de esta consulta:"));
+
 
 let margenFacturacion;
 
-//Verifica en qué categoría estás.
-if(facturacion < limites[0]){
-    alert(`estás en la ${categorias[0]}`);
-    margenFacturacion = calcularMargen(limites[0],facturacion);
-    alert(`Te queda un margen de facturación para mantener la categoría actual de: ${margenFacturacion}`);
-} else if(facturacion > limites[3] && facturacion < limites[4]){
-    alert("Estás en la última categoría");
-    margenFacturacion = calcularMargen(limites[4],facturacion);
-    alert(`Te queda un margen de facturación para mantener la categoría actual de: ${margenFacturacion}`);
-} else{
-    for(let i=0; i<limites.length; i++){
-        if(facturacion>=limites[i] && facturacion<limites[i+1]){
-            alert(`estás en la ${categorias[i+1]}`);
-            margenFacturacion = calcularMargen(limites[i+1],facturacion);
+class Cliente{
+    constructor(nombre, condicionFiscal, facturacion){
+        this.nombre = nombre;
+        this.condicionFiscal = condicionFiscal;
+        this.facturacion = facturacion;
+    }
+
+    calcularMargen(limite, facturacion){
+        let calculo = limite - facturacion;
+
+        return calculo;
+    }
+    
+    verificarCategoría(){
+        if(this.facturacion < limites[0]){
+            alert(`estás en la ${categorias[0]}`);
+            let margenFacturacion = this.calcularMargen(limites[0],this.facturacion);
             alert(`Te queda un margen de facturación para mantener la categoría actual de: ${margenFacturacion}`);
+        } else if(this.facturacion > limites[3] && this.facturacion < limites[4]){
+            alert("Estás en la última categoría");
+            let margenFacturacion = this.calcularMargen(limites[4],this.facturacion);
+            alert(`Te queda un margen de facturación para mantener la categoría actual de: ${margenFacturacion}`);
+        } else{
+            for(let i=0; i<limites.length; i++){
+                if(this.facturacion>=limites[i] && this.facturacion<limites[i+1]){
+                    alert(`estás en la ${categorias[i+1]}`);
+                    let margenFacturacion = this.calcularMargen(limites[i+1],this.facturacion);
+                    alert(`Te queda un margen de facturación para mantener la categoría actual de: ${margenFacturacion}`);
+                }
+            }
         }
     }
+
+    verificarRI(){
+        if(this.facturacion >= limites[4]){
+            alert("Eres responsable inscripto o deberías pasar a ese régimen impositivo");
+            netoCompras = prompt("Ingrese el importe NETO de IVA de sus compras para calcular el IVA crédito fiscal.");
+            netoVentas = prompt("Ingrese el importe NETO de IVA de sus ventas para calcular el IVA débito fiscal.");
+        } else{
+            alert("No eres responsable inscripto");
+        }
+    }
+
+    calcularIVA(){
+        let IVACompras = netoCompras*0.21;
+        let IVAVentas = netoVentas*0.21;
+    
+        let saldoTecnico = IVAVentas - IVACompras;
+        
+        alert(`Su Saldo Técnico mensual de IVA es el siguiente: ${saldoTecnico}`);
+        alert("Sólo debe restarle las percepciones y retenciones correspondientes para obtener su posición mensual de IVA.");
+    }
+    
 }
 
-//Calcula el margen de facturación restante para la siguiente categoría
-function calcularMargen(limite,facturacion){
-    let calculo = limite - facturacion;
 
-    return calculo;
+const cliente1 = new Cliente(nombre, condicionFiscal, facturacion);
+
+cliente1.verificarCategoría();
+cliente1.verificarRI();
+if(cliente1.condicionFiscal.toUpperCase() === "RESPONSABLE INSCRIPTO" && cliente1.facturacion >= limites[4]){
+    cliente1.calcularIVA();
 }
-
-if(facturacion >= limites[4]){
-    alert("Eres responsable inscripto o deberías pasar a ese régimen impositivo");
-    netoCompras = prompt("Ingrese el importe NETO de IVA de sus compras para calcular el IVA crédito fiscal.");
-    netoVentas = prompt("Ingrese el importe NETO de IVA de sus ventas para calcular el IVA débito fiscal.")
-}
-
-//Calcula el saldo técnico de la delcaración juarada de IVA
-function calcularIVA(){
-    let IVACompras = netoCompras*0.21;
-    let IVAVentas = netoVentas*0.21;
-
-    let resultado = IVAVentas - IVACompras;
-    return resultado;
-}
-
-let saldoTecnico = calcularIVA();
-
-if(saldoTecnico){
-    alert(`Su Saldo Técnico mensual de IVA es el siguiente: ${saldoTecnico}`);
-    alert("Sólo debe restarle las percepciones y retenciones correspondientes para obtener su posición mensual de IVA.");
-}
-
